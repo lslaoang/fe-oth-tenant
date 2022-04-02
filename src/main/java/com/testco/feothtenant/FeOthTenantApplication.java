02f4bc0330918953326c074edf916f05a0bd1dd4
Laurel Lao Ang <laurellaoang@gmail.com>
2022-04-02 21:21:00 +0800

    authentication flow added

--- src/main/java/com/testco/feothtenant/FeOthTenantApplication.java
+++ src/main/java/com/testco/feothtenant/FeOthTenantApplication.java
@@ -2,12 +2,19 @@ package com.testco.feothtenant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FeOthTenantApplication {
+public class FeOthTenantApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FeOthTenantApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FeOthTenantApplication.class);
     }
 
 }
